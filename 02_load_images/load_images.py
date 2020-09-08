0a4cbba0d1a4f6212d923779308bab2b2e2005ee
Minsu Kim <alstn9008@naver.com>
2020-09-08 16:16:11 +0900

    add a load image code

---
+++ 02_load_images/load_images.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+#Get the path to the current file
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+#Change the working directory
+os.chdir(cwd)
+
+#Load an image
+img = cv2.imread('messi5.jpg')
+
+# Display the image ina a window
+cv2.imshow('image', img)
+
+# Wait for a key to be pressed
+cv2.waitKey(0)
+
+# Destroy all windows
+cv2.destroyAllWindows()
